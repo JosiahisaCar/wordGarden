587e7ae12abf434af96309b5d22f11fb9d41eb60
Josiah <josiah.carver@icloud.com>
2025-12-03 00:43:54 -0500

    Initial Commit

---
+++ wordGarden/wordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordGardenApp.swift
+//  wordGarden
+//
+//  Created by Josiah Carver on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct wordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
